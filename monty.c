a6aedf3b8f9a7aad85931ab6f42a550a04a057f1
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-19 15:54:22 -0700

    Open file

--- monty.c
+++ monty.c
@@ -8,11 +8,18 @@
  */
 int main(int ac, char **av)
 {
-	(void) av;
+	FILE *file;
 
 	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE : monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(av[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 
