d4a68228e9e8a581153262063278ce5c4fc39fef
Yassine AIT MENSOUR <aitmensouryassine@outlook.com>
2023-06-20 17:42:22 +0000

    Implement mul function

--- getop_func.c
+++ getop_func.c
@@ -17,6 +17,7 @@ void (*get_op_func(char *opcode))(stack_t **stack, unsigned int line_number)
 		{"nop", _nop},
 		{"sub", _sub},
 		{"div", _div},
+		{"mul", _mul},
 		{NULL, NULL}
 	};
 	int i = 0;
--- op_func1.c
+++ op_func1.c
@@ -119,3 +119,37 @@ void _div(stack_t **stack, unsigned int line_number)
 		current = current->next;
 	}
 }
+
+/**
+ * _mul - multiplies the two top elements of the stack
+ * @stack: a pointer to the first node of the stack
+ * @line_number: the line number
+ */
+
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current, *prev = NULL;
+
+
+	if (stack_len(*stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		free_all(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		if (current->next == NULL)
+		{
+			prev->n = prev->n * current->n;
+			prev->next = NULL;
+			free(current);
+			break;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
